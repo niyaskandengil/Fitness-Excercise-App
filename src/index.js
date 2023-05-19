ab0c59283c5d163db3e29c483736d439868a2c97
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-19 12:43:30 +0530

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import {createRoot}  from "react-dom/client";
+import App from './App'
+createRoot(document.getElementById("root")).render(<App/>);
