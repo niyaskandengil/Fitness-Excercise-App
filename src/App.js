f373ba701547d2ee8469cc11e42054e3ab3fce44
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-20 13:27:38 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/exercise/:id" element={<ExerciseDetails />} />
       </Routes>
+      <Footer/>
     </Box>
   )
 }
