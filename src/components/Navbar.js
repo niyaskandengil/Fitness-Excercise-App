21369c1c97075bed50b7e5804bd53fd71f328ff6
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-20 21:48:28 +0530

    Navbar 09.48pm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,12 @@ const Navbar = () => {
         <Link to="/">
           <img src={Logo} alt="logo" style={{width: '48px',height:'48px',margin:'0 20px'}} />
         </Link>
-        <Stack direction="row">
+        <Stack 
+           direction="row"
+           gap="40px"
+           fontSize="24px"
+           alignItems="flex-end"
+           >
             <Link to="/" style={{textDecoration:'none',color:'#3A1212' , borderBottom:'3px solid #FF2625 '}}>Home</Link>
             <a href="#exercises" style={{textDecoration:'none',color:'#3A1212'}}>Exercise</a>
         </Stack>
