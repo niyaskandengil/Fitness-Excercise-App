243087ba8fbcc39406a392f7592baecf06222c0b
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-20 13:14:16 +0530

    01.14PM

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import './App.css';
+import './App.css'; // import css file
+import { Route,Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import ExerciseDetails from './pages/ExerciseDetails';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
-    <div>Appxz</div>
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+    </Box>
   )
 }
 
