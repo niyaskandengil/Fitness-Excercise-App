20b9f5fe17a911a4b4701b2a62aa9761bbef9106
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-20 13:48:48 +0530

    01.33pm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './App.css'; // import css file
-import { Route,Routes } from 'react-router-dom';
+import { Route,Routes, useRoutes } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from './pages/Home';
 import ExerciseDetails from './pages/ExerciseDetails';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <Box width="400px">
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import {createRoot}  from "react-dom/client";
 import App from './App'
-createRoot(document.getElementById("root")).render(<App/>);
+import React from 'react'
+import { BrowserRouter } from "react-router-dom";
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+    <BrowserRouter>
+       <App/>
+    </BrowserRouter>
+);
