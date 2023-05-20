57348907e0fa6400e76c9ef3045e406c2af7df01
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-20 22:00:45 +0530

    Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import React from 'react';
+import { Box } from "@mui/material";
+import { Search } from "@mui/icons-material";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
+    <Box>
+      <HeroBanner/>
+      <SearchExcercises/>
+      <Excercises/>
+    </Box>
   );
 }
 
