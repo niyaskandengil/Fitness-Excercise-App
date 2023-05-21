b1f803d476edf884160540461395dca2435accb2
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 12:33:41 +0530

    Hero Banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,9 @@ import { useState } from "react";
 import React from 'react';
 import { Box } from "@mui/material";
 import { Search } from "@mui/icons-material";
+import HeroBanner from "../components/HeroBanner";
+import SearchExcercises from "../components/SearchExcercises";
+import Excercises from "../components/Excercises";
 
 const Home = () => {
   return (
