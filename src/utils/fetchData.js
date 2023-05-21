d9ac67966c14421f1405f1076213c3c43c1d6fc0
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 20:26:43 +0530

    #

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -14,10 +14,8 @@ export const fetchData = async (url, options) => {
 }
 
 export const youtubeOptions = {
-    method: 'GET',
-    url: 'https://youtube-search-and-download.p.rapidapi.com/channel/about',
     headers: {
-      'X-RapidAPI-Key': '37e67c2e7emsh1eae83ee2f16daap1f485ajsn71b7292e51cb',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
     }
   };
