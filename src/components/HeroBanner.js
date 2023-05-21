0cb843ebb0835f5eaff7f3de6112b5444dcdaa86
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 22:11:03 +0530

    #

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
-import HeroBannerImage from "../assets/images/banner.png";
+import HeroBannerImage from "../assets/images/banner1.png";
 
 const HeroBanner = () => (
   <Box
