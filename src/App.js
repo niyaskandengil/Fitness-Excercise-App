8122f53c23a82ca9e0ddcf95be338e123025192b
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 19:51:59 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import './App.css'; // import css file
-import { Route,Routes, useRoutes } from 'react-router-dom';
+import { Route,Routes} from 'react-router-dom';
 import { Box } from '@mui/material';
 import Home from './pages/Home';
-import ExerciseDetails from './pages/ExerciseDetails';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ExerciseDetail from './pages/ExerciseDetails';
 
-const App = () => {
-  return (
-    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetails />} />
-      </Routes>
-      <Footer/>
-    </Box>
-  )
-}
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
+);
 
 export default App
