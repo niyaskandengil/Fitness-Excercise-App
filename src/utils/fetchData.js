fab5e6718beed6f9a8858099ea2a3ff4f275391d
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 12:50:45 +0530

    fetchdata

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,3 +1,12 @@
+export const exercisesOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+  };
+
+
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
