49a89a5133f014d620aa2542b61f879fc219a3bc
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 12:53:08 +0530

    fetch data

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,7 +1,7 @@
 export const exercisesOptions = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': '',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
