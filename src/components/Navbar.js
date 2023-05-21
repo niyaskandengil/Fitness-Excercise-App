b111a79a3dc4a29dbea6a0c9adf9ea9463b009c4
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-05-21 23:08:35 +0530

    #

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material';
 
-import Logo from '../assets/images/Logo.png';
+// import Logo from '../assets/images/Logo.png';
 import Logo1 from '../assets/images/Logo1.png';
 
 const Navbar = () => {
